73895b9d91937c70e59e098a65e6d2d3ba8b6a49
Carlos <cflo120809@gmail.com>
2022-08-15 12:02:27 -0500

    actualizacion ejercicio_4

--- Python/Ejercicio_4.py
+++ Python/Ejercicio_4.py
@@ -1,15 +1,15 @@
-i=2
+#i=2
 a=input ("Como te llamas? ")
 numero = int(input ("Dame un numero: "))
-if numero == i:
+#if numero == i:
  # else:
-  print ("HOLA", a)
+print (("HOLA", a)*numero)
   #print (i)
-else:
+#else:
 # if i == 0:
 #else:
 #i = i+1
 #print (b)
-  print (i)
-  i = i+1
+ # print (i)
+  #i = i+1
 
