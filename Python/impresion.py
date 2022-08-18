b0d4f43a968f0c5217e1907f31f9e551b6cdadc9
Carlos <cflo120809@gmail.com>
2022-08-18 18:29:30 -0500

    soluciones ejercicios_2_18ago2022

---
+++ Python/impresion.py
@@ -0,0 +1,23 @@
+#print(f"¡Hola \n Mundo!")
+#print(f"¡Hola Mundo!")
+#print("¡Hola Mundo!")
+
+n = int (input ("Introduce un numero: "))
+b = str (input ("Como te llamas? "))
+#print (i)
+for i in range (n):
+    if i <= n:
+        print(b, end="\n")
+    else:
+            print(b)
+            
+a = int (input ("Introduce un numero: "))
+c = str (input ("Como te llamas? "))
+print ("\n",c*a)
+#for i in range (n):
+ #   if i <= n:
+  #      print(b, end="\n")
+   # else:
+    #        print(b)
+
+    
