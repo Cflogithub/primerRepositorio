b913cc6b676380d49422c5d3e0b28acc8fc5dea9
Carlos <cflo120809@gmail.com>
2022-08-20 22:38:25 -0500

    ejercicios 20ago2022_1

---
+++ EDC_AnalisisDeDatos-main/ejercicios/pruebas con bucles.py
@@ -0,0 +1,19 @@
+#Ejemplo, i tomara valores de 0 a 9, el valor incia es 0 por defecto
+# i tomara valores menores a 10
+#for i in range(10):
+#    print(i)
+
+# Ejemplo con todos sus componentes
+#for i in range(6, 20, 2):
+#    print(i)
+
+# Ejemplo con el paso -1
+#for i in range(10, 0, -1):
+#    print(i)
+
+# Ejemplo
+continuar = 1
+while (continuar):
+    continuar = int(input("Introduce 1 o 0 "))
+    print("Hola mundo")
+    print(continuar)
